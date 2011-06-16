d6b0ad249781063fc926444346b4719d97b5c85a
Jack Reilly <jackdreilly@gmail.com>
2011-06-16 03:13:57 -0700

    again commit

---
+++ VoyeurHero/backend/search_indexes.py
@@ -0,0 +1,21 @@
+from haystack import indexes
+from models import *
+
+    
+class VHCategoryIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    # We add this for autocomplete.
+    content_auto = indexes.EdgeNgramField(model_attr='title')
+    
+    def get_model(self):
+        return VHCategory
+        
+class VHPostIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    post_date = indexes.DateTimeField(model_attr='post_date')
+    caption = indexes.CharField(model_attr='caption')
+    # We add this for autocomplete.
+    content_auto = indexes.EdgeNgramField(model_attr='title')
+    
+    def get_model(self):
+        return VHPost
