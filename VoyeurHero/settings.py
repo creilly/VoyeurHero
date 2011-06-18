301df63776c476299578da802625dc9ccf7c0208
Jack Reilly <jackdreilly@gmail.com>
2011-06-17 22:38:28 -0700

    admin settings

--- VoyeurHero/settings.py
+++ VoyeurHero/settings.py
@@ -59,7 +59,7 @@ USE_L10N = True
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/vh/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
