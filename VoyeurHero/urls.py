66efe12484f4ad352e40a80f39a74860accdbfe0
= <=>
2011-06-20 15:37:00 -0700

    voting added

--- VoyeurHero/urls.py
+++ VoyeurHero/urls.py
@@ -3,9 +3,17 @@ from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+from django.conf.urls.defaults import *
+from voting.views import xmlhttprequest_vote_on_object
+from backend.models import VHPost
+
+vhpost_dict = {
+    'model': VHPost,
+}
 
 urlpatterns = patterns('',
     # Examples:
+    (r'^post_vote/(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$', xmlhttprequest_vote_on_object, vhpost_dict),
     (r'^accounts/', include('registration.urls')),
     (r'^profiles/', include('profiles.urls')),
     (r'^comments/', include('django.contrib.comments.urls')),
