75b8da8056205c8c08e8e4d5f60fbb64df10ce21
Jack Reilly <jackdreilly@gmail.com>
2011-06-21 20:12:45 -0700

    server changes?

--- VoyeurHero/backend/models.py
+++ VoyeurHero/backend/models.py
@@ -18,6 +18,7 @@ class VHTag(models.Model):
 		return self.name
 
 class VHPost(models.Model):
+	user = User()
 	picture = models.ImageField(upload_to='post_images',verbose_name = 'Post Picture', null = True,blank = True,default='post_images/mystery-man.jpg')
 	caption = models.CharField(max_length=10000, verbose_name = 'Caption for Picture')
 	post_date = models.DateTimeField(auto_now_add=True,verbose_name = 'Posted Date')
